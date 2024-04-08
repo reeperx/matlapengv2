56cec8421493869dbfefc58c6b9d08785fbc6fc3
reeperx10 <acebany080@gmail.com>
2024-04-08 08:37:54 +0200

    more changes

--- app/(watch-course)/(routes)/view-course/_components/ChapterNav.jsx
+++ app/(watch-course)/(routes)/view-course/_components/ChapterNav.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { PlayCircle, PauseCircle, CheckCircle2 } from "lucide-react";
 
-function ChapterNav({course, userCourse}) {
+function ChapterNav({ course, userCourse, setActiveChapter }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    setActiveChapter(course?.chapter[0]);
+  }, []);
+
   return (
     <div>
       <div className="border-b p-5">
         <h2 className="font-medium text-[20px]">{course.name}</h2>
-        <h2 className='text-gray-500 text-[14px]'>{course.author}</h2>
+        <h2 className="text-gray-500 text-[14px]">{course.author}</h2>
+      </div>
+      <div>
+        {course.chapter.map((chapter, index) => (
+          <div
+            onClick={() => {
+              setActiveIndex(index);
+              setActiveChapter(chapter);
+            }}
+            key={index}
+            className={`flex gap-2 text-gray-500 text-[16px] px-5 p-4 cursor-pointer hover:bg-orange-200 ${
+              activeIndex == index ? "bg-amber-100 text-orange-700" : null
+            }`}
+          >
+            {activeIndex == index ? (
+              <PauseCircle height={25} width={25} />
+            ) : (
+              <PlayCircle height={25} width={25} />
+            )}
+            <h2 className="line-clamp-2">{chapter.name}</h2>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default ChapterNav
+export default ChapterNav;
--- app/(watch-course)/(routes)/view-course/_components/FullVideoPlayer.jsx
+++ app/(watch-course)/(routes)/view-course/_components/FullVideoPlayer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-function FullVideoPlayer() {
+function FullVideoPlayer({activeChapter}) {
   return (
-    <div>FullVideoPlayer</div>
+    <div className='p-5'>
+      <video
+        key={activeChapter?.video?.url}
+        width="1500"
+        height="250"
+        controls
+        controlsList="nodownload"
+      >
+        <source src={activeChapter?.video?.url} type="video/mp4" />
+      </video>
+    </div>
   )
 }
 
