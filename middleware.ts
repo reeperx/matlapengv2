757c7e8711247bc16e233352389f630b7207399a
reeperx10 <acebany080@gmail.com>
2024-04-08 11:17:59 +0200

    changes

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-    publicRoutes:["/dashboard", "/course-preview/(.*)"]
+    publicRoutes:["/browse", "/course-preview/(.*)"]
 });
 
 export const config = {
